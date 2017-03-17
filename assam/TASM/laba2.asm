1bd65ff6269433b501879a0d2266786e1f78efa7
elBroom <Zarina1458@yandex.ru>
2017-03-18 00:35:56 +0300

    add revert byte to laba2

--- assam/TASM/laba2.asm
+++ assam/TASM/laba2.asm
@@ -1,8 +1,10 @@
 ;dd palindromes
 .model small
 .data
-    w dd 069F531Fh
-    result db 0
+    w dd 010FFFFFEh
+    result db 0 ;2
+    buf db 0
+    head db ?
 .code
 .486
     mov ax, @data
@@ -11,6 +13,7 @@
     xor ebx, ebx ; eax=0
     xor ecx, ecx ; ecx=0
     xor edx, edx ; edx=0
+
     mov eax, w
     mov cl, 4
 calc: ; start loop
@@ -18,6 +21,22 @@ calc: ; start loop
     mov dl, al
     shr dl, 4 ; head
     and bl, 0Fh ; tail
+    mov head, dl; save head
+    mov buf, cl; save counter
+
+    xor cx, cx 
+    xor dx, dx
+    mov cl, 4
+    revert: ; revert tail
+        shl dl, 1
+        shr bl, 1
+        jnc endRev ;carry flag
+        add dl, 1
+    endRev:
+        loop revert
+
+    mov cl, buf
+    mov bl, head
     xor bl, dl
     jnz endLoop
     inc result
